cda570adff325a742617365e99787f9d1de8f15b
Kilaparthi <848063@cognizant.com>
2020-02-07 10:46:26 +0530

    set expiration time to 1 hour

--- src/main/java/com/capstone/jwt/config/CapstoneJwtTokenGeneration.java
+++ src/main/java/com/capstone/jwt/config/CapstoneJwtTokenGeneration.java
@@ -29,7 +29,7 @@ public class CapstoneJwtTokenGeneration implements Serializable {
 	private String doGenerateToken(Map<String, Object> claims, String username) {
 		
 		final Date createdDate = new Date();
-		final Date expirationDate = new Date(createdDate.getTime() + 100 * 1000);
+		final Date expirationDate = new Date(createdDate.getTime() + 60 * 60 * 1000);
 		return Jwts.builder()
 				.setClaims(claims)
 				.setSubject(username)
